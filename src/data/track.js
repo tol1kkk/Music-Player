4f68b970528fc13fd2e7d7e38ba35b61d9e0c4f7
Дрибуленда Андрій <andriyshef10@gmail.com>
2026-04-11 10:14:13 +0300

    feat: завантажені пісні

--- src/data/track.js
+++ src/data/track.js
@@ -1,50 +1,50 @@
 export const tracks = [
     {
         id: 1,
-        title: "Track 1",
-        artist: "Artist 1",
+        title: "Spirit Crusher",
+        artist: "Death",
         album: "Album 1",
-        cober: "",
-        src: ""
+        cober: "https://i.pinimg.com/1200x/28/be/58/28be581c5760eae372b13c739c9f6fbf.jpg",
+        src: "https://mp3tourl.com/audio/1775890745312-e3c558aa-e00b-409b-86e9-7b6057efd93d.m4a"
     },
     {
         id: 2,
-        title: "Track 2",
-        artist: "Artist 2",
+        title: "Парфум",
+        artist: "second part",
         album: "Album 2",
-        cober: "",
-        src: ""
+        cober: "https://images.genius.com/f07ca00bf428eb4aca9d51895fea6f4a.1000x1000x1.png",
+        src: "https://mp3tourl.com/audio/1775890773267-a494354f-ab9d-4630-914b-3f01b2d0dd8e.m4a"
     },
     {
         id: 3,
-        title: "Track 3",
-        artist: "Artist 3",
+        title: "the chair",
+        artist: "the 69 eyes",
         album: "Album 3",
-        cober: "",
-        src: ""
+        cober: "https://i.pinimg.com/736x/ac/5b/41/ac5b4128b9211184d4fcf8b57725305b.jpg",
+        src: "https://mp3tourl.com/audio/1775890803439-eb4320f2-5218-4cb6-88ad-41ded7fa6f95.m4a"
     },
     {
         id: 4,
-        title: "Track 4",
-        artist: "Artist 4",
+        title: "solo",
+        artist: "future ",
         album: "Album 4",
-        cober: "",
-        src: ""
+        cober: "https://i.pinimg.com/1200x/98/44/da/9844daff0fb743f00ba54b2d727a503f.jpg",
+        src: "https://mp3tourl.com/audio/1775890719764-baf389ef-fb96-44c7-8060-6f6b6d5a499c.m4a"
     },
     {
         id: 5,
-        title: "Track 5",
-        artist: "Artist 5",
+        title: "rolling stone",
+        artist: "brent Feiyaz",
         album: "Album 5",
-        cober: "",
-        src: ""
+        cober: "https://i.pinimg.com/736x/7d/3f/42/7d3f4237ed0f1dcfb03d7a459af46829.jpg",
+        src: "https://mp3tourl.com/audio/1775890628304-5f1722fb-d206-435c-9fb2-62edae160588.m4a"
     },
     {
         id: 6,
-        title: "Track 6",
-        artist: "Artist 6",
+        title: "Feel good",
+        artist: "clara la san",
         album: "Album 6",
-        cober: "",
-        src: ""
+        cober: "https://i.pinimg.com/736x/5a/2c/90/5a2c907907574033dc19021052afc0e8.jpg",
+        src: "https://mp3tourl.com/audio/1775890684508-f8c0f9d0-c08f-417a-8cef-d80dbe9624bc.m4a"
     },
 ];
