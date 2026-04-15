c4f74719536a81f12c90a4d9e032652d0c0ab8c4
Дрибуленда Андрій <andriyshef10@gmail.com>
2026-04-15 21:47:49 +0300

    all is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-<BrowserRouter> 
-    <Navbar /> 
-    <Routes> 
-      <Route path='/' element={<Home/>}/> 
-      <Route path='/search' element={<Search/>}/> 
-      <Route path='/favorites' element={<Favorites/>}/> 
-    </Routes> 
-    <PlayerBar />
-  </BrowserRouter> 
+import Navbar from "./components/Navbar/Navbar.jsx";
+import tracks from "./data/track.js";
+export default function App() {
+      return (
+        <div className="container">
+          <TrackCard/>
+          <main className="main">
+            <div className="header_types"></div>
+            <section className="music_list">
+              
+            </section>
+            <Navbar/>
+          </main>
+        </div>
+        // <BrowserRouter> 
+        //   <Navbar /> 
+        //   <Routes> 
+        //     <Route path='/' element={<Home/>}/> 
+        //     <Route path='/search' element={<Search/>}/> 
+        //     <Route path='/favorites' element={<Favorites/>}/> 
+        //   </Routes> 
+        //   <PlayerBar />
+        // </BrowserRouter> 
+      );
+}
