8b65f6579b53c09d3be69fa664d90f5d45ce236f
Дрибуленда Андрій <andriyshef10@gmail.com>
2026-04-15 21:55:57 +0300

    save local changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import Navbar from "./components/Navbar/Navbar.jsx";
+import tracks from {tracks} from "./data/track.js";
 export default function App() {
       return (
-        <BrowserRouter> 
-          <Navbar /> 
-          <Routes> 
-            <Route path='/' element={<Home/>}/> 
-            <Route path='/search' element={<Search/>}/> 
-            <Route path='/favorites' element={<Favorites/>}/> 
-          </Routes> 
-          <PlayerBar />
-        </BrowserRouter> 
+        <div className="container">
+          <TrackCard/>
+          <main className="main">
+            <div className="header_types"></div>
+            <section className="music_list">
+              
+            </section>
+            <Navbar/>
+          </main>
+        </div>
+        // <BrowserRouter> 
+        //   <Navbar /> 
+        //   <Routes> 
+        //     <Route path='/' element={<Home/>}/> 
+        //     <Route path='/search' element={<Search/>}/> 
+        //     <Route path='/favorites' element={<Favorites/>}/> 
+        //   </Routes> 
+        //   <PlayerBar />
+        // </BrowserRouter> 
       );
 }
