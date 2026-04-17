32eaf674772a4992b5a3368c5dfe29d38cb73cf8
Дрибуленда Андрій <andriyshef10@gmail.com>
2026-04-17 19:08:01 +0300

    yess

--- src/components/trackCard/TrackCard.jsx
+++ src/components/trackCard/TrackCard.jsx
@@ -1,7 +1,9 @@
 export default function TrackCard() {
   return (
     <div className="track-card">
-        
+        <div className="trackCard-header">
+            <button className="options">пш</button>
+        </div>
     </div> 
   );
 }
