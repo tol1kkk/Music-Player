a45bc8a2038841e0d1e0a7839f81e7dc8367b2e1
Дрибуленда Андрій <andriyshef10@gmail.com>
2026-04-17 19:12:45 +0300

    structure added

--- src/components/trackCard/TrackCard.jsx
+++ src/components/trackCard/TrackCard.jsx
@@ -2,7 +2,25 @@ export default function TrackCard() {
   return (
     <div className="track-card">
         <div className="trackCard-header">
-            <button className="options">пш</button>
+            <button className="options">
+                Зараз
+            </button>
+             <button className="options">
+                Черга
+            </button>
+             <button className="options">
+               Текст
+            </button>
+        </div>
+        <div className="trackCard-body">
+            <img src="./public/favicon.svg" alt="" className="track-img"/>
+            <div className="track-info">
+                <h1 className="trackName">Buildings in fire</h1>
+                <h3 className="song-author">blablabla</h3>
+            </div>
+            <div className="controls">
+                
+            </div>
         </div>
     </div> 
   );
